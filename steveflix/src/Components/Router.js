b417ced4122b1af039ab8dd7b3083e273daccc55
stevesung <steveysung91@gmail.com>
2019-04-17 14:12:47 +0900

    react router2

--- steveflix/src/Components/Router.js
+++ steveflix/src/Components/Router.js
@@ -1,17 +1,22 @@
 import React from "react";
-import { HashRouter as Router, Route } from "react-router-dom";
+import {
+  BrowserRouter as Router,
+  Route,
+  Redirect,
+  Switch
+} from "react-router-dom";
 import Home from "Routes/Home";
 import TV from "Routes/TV";
 import Search from "Routes/Search";
 
 export default () => (
   <Router>
-    <>
-      {/* 라우터 생성 */}
+    <Switch>
       <Route path="/" exact component={Home} />
-      {/* 어떤 경로로 갔을때 어떤 컴포넌트를 보여줄것인가 */}
       <Route path="/tv" exact component={TV} />
       <Route path="/search" exact component={Search} />
-    </>
+      <Redirect from="*" to="/" />
+      {/* 일치하는 Route가 하나도 없다면 어느페이지든지 "/"  여기로 보내라 */}
+    </Switch>
   </Router>
 );
