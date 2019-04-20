8824a17ab7beb28e1d687b059d46755408f7caa1
stevesung <steveysung91@gmail.com>
2019-04-20 11:30:57 +0900

    CSS4
    styled-reset

--- steveflix/src/Components/App.js
+++ steveflix/src/Components/App.js
@@ -1,5 +1,6 @@
 import React, { Component } from "react";
 import Router from "Components/Router";
+import globalStyles from "./GlobalStyles";
 
 // index.js가 있기때문에 이렇게 써도 해당폴더가서 가져올수 있음
 // ./이런경로보다 이렇게써야함
@@ -9,6 +10,7 @@ class App extends Component {
     return (
       <>
         <Router />
+        <globalStyles />
       </>
     );
     {
