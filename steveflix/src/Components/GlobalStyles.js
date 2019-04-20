22ab4ed8edc55dc1c97feb73b6a3ded5b04178c7
stevesung <steveysung91@gmail.com>
2019-04-20 11:39:47 +0900

    CSS5
    header css

--- steveflix/src/Components/GlobalStyles.js
+++ steveflix/src/Components/GlobalStyles.js
@@ -4,7 +4,7 @@ import reset from "styled-reset";
 const globalStyles = createGlobalStyle`
     ${reset};
     a{
-        text-decoration : none;
+        text-decoration:none;
         color:inherit;
     }
     *{
@@ -12,9 +12,10 @@ const globalStyles = createGlobalStyle`
     }
     body{
         font-family:-apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, Oxygen, Ubuntu, Cantarell, 'Open Sans', 'Helvetica Neue', sans-serif;
-        font-size:14px;
-        background-color : rgba(20,20,20,1);
-
+        font-size:12px;
+        background-color:rgba(20, 20, 20, 1);
+        color:white;
+        padding-top:50px;
     }
 
 `;
