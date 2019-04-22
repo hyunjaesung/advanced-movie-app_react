c951302531660dc07a18583ecf9cf35abaa5826f
stevesung <steveysung91@gmail.com>
2019-04-22 13:46:23 +0900

    api

--- README.md
+++ README.md
@@ -8,3 +8,13 @@ Learning React and ES6 by building Movie Discovery APP
 - [ ] TV Shows
 - [ ] Search
 - [ ] Detail
+
+## API Verbs
+
+- [x] Now Playing
+- [x] Upcoming (TV, Movie)
+- [x] Top Rated (TV, Movie)
+- [x] Popular (TV, Movie)
+- [x] Airing Today (TV)
+- [x] Detail (TV, Movie)
+- [x] Search(TV, Movie)
---
+++ steveflix/src/api.js
@@ -0,0 +1,45 @@
+import axios from "axios";
+
+const api = axios.create({
+  baseURL: "https://api.themoviedb.org/3/",
+  params: {
+    api_key: "a1093d024b297a718e88b5a7b969aac8",
+    language: "en-US"
+  }
+});
+
+export const movieApi = {
+  nowPlaying: () => api.get("moivie/now_playing"),
+  upcoming: () => api.get("moivie/upcoming"),
+  popular: () => api.get("movie/popular"),
+  movieDetail: id =>
+    api.get(`movie/${id}`, {
+      params: {
+        append_to_response: "videos"
+      }
+    }),
+  search: term =>
+    api.get("search/movie", {
+      params: {
+        query: encodeURIComponent(term) // URI로 인코딩필요
+      }
+    })
+};
+
+export const TVApi = {
+  topRated: () => api.get("tv/top_rated"),
+  airingToday: () => api.get("tv/airing_today"),
+  popular: () => api.get("tv/popular"),
+  showDetail: id =>
+    api.get(`tv/${id}`, {
+      params: {
+        append_to_response: "videos"
+      }
+    }),
+  search: term =>
+    api.get("search/tv", {
+      params: {
+        query: encodeURIComponent(term) // URI로 인코딩필요
+      }
+    })
+};
