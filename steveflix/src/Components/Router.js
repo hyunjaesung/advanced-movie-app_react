11e46cd39dd051ca163ae730169846bd6c708ba1
stevesung <steveysung91@gmail.com>
2019-04-23 13:14:41 +0900

    DetailContainer

--- steveflix/src/Components/Router.js
+++ steveflix/src/Components/Router.js
@@ -5,11 +5,11 @@ import {
   Redirect,
   Switch
 } from "react-router-dom";
+import Header from "Components/Header";
 import Home from "Routes/Home";
 import TV from "Routes/TV";
 import Search from "Routes/Search";
-import Header from "Components/Header";
-import Detail from "Components/Detail";
+import Detail from "Routes/Detail";
 
 export default () => (
   <Router>
@@ -19,8 +19,8 @@ export default () => (
         <Route path="/" exact component={Home} />
         <Route path="/tv" exact component={TV} />
         <Route path="/search" exact component={Search} />
-        <Route path="/movie/:id" compnent={Detail} />
-        <Route path="/show/:id" compnent={Detail} />
+        <Route path="/movie/:id" component={Detail} />
+        <Route path="/show/:id" component={Detail} />
         <Redirect from="*" to="/" />
         {/* 일치하는 Route가 하나도 없다면 어느페이지든지 "/"  여기로 보내라 */}
       </Switch>
--- steveflix/src/Routes/Detail/DetailContainer.js
+++ steveflix/src/Routes/Detail/DetailContainer.js
@@ -1,14 +1,57 @@
 import React from "react";
 import DetailPresenter from "./DetailPresenter";
+import { movieApi, TVApi } from "../../api";
 
 export default class extends React.Component {
-  state = {
-    result: null,
-    error: null,
-    loading: true
-  };
+  constructor(props) {
+    super(props);
+    const {
+      location: { pathname }
+      // 생성자클래스라서
+      // pathname 존재하지않는다
+    } = props;
+    this.state = {
+      result: null,
+      error: null,
+      loading: true,
+      isMovie: pathname.includes("/movie/")
+    };
+  }
 
-  // todo : get api error 처리
+  async componentDidMount() {
+    const {
+      match: {
+        params: { id }
+      },
+      history: { push },
+      location: { pathname }
+    } = this.props;
+
+    const { isMovie } = this.state; // state에 넣은 값을 가지고옴
+
+    const parsedID = parseInt(id);
+
+    if (isNaN(parsedID)) {
+      return push("/");
+    }
+
+    let result = null;
+
+    try {
+      if (isMovie) {
+        const request = await movieApi.movieDetail(parsedID);
+        result = request.data;
+      } else {
+        const request = await TVApi.showDetail(parsedID);
+        result = request.data;
+      }
+      console.log(result);
+    } catch {
+      this.setState({ error: "Can't find anything." });
+    } finally {
+      this.setState({ loading: false, result });
+    }
+  }
 
   render() {
     const { result, error, loading } = this.state;
