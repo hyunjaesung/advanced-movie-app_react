5ee570cda0c04afcf373855375d35c3ba426e36b
stevesung <steveysung91@gmail.com>
2019-04-24 14:51:38 +0900

    HomePresenter and Section Component

--- steveflix/src/Routes/Home/HomePresenter.js
+++ steveflix/src/Routes/Home/HomePresenter.js
@@ -1,17 +1,37 @@
 import React from "react";
-import ProsTypes from "prop-types";
+import propTypes from "prop-types";
 import styled from "styled-components";
+import Section from "Components/Section";
+
+const Container = styled.div`
+  padding: 0px 10px;
+`;
 
 const HomePresenter = ({ nowPlaying, upcoming, popular, error, loading }) =>
-  null;
+  loading ? null : (
+    <Container>
+      {nowPlaying && nowPlaying.length > 0 && (
+        <Section title="Now Playing">
+          {nowPlaying.map(movie => movie.title)}
+        </Section>
+      )}
+      {/* 트리플 체크 필요!, children은 title처럼 주지않고 태그사이의 값을 받음 */}
+      {upcoming && upcoming.length > 0 && (
+        <Section title="Upcoming">{upcoming.map(movie => movie.title)}</Section>
+      )}
+      {popular && popular.length > 0 && (
+        <Section title="Popular">{popular.map(movie => movie.title)}</Section>
+      )}
+    </Container>
+  );
 
-HomePresenter.ProsTypes = {
+HomePresenter.propTypes = {
   // props의 자료형 정해줌
-  nowPlaying: ProsTypes.array,
-  upcoming: ProsTypes.array,
-  popular: ProsTypes.array,
-  error: ProsTypes.bool.isRequired,
-  loading: ProsTypes.string
+  nowPlaying: propTypes.array,
+  upcoming: propTypes.array,
+  popular: propTypes.array,
+  error: propTypes.bool.isRequired,
+  loading: propTypes.string
 };
 
 export default HomePresenter;
