031c358c4604067e92e7f4db0682b03610e9c133
steve <steveysung91@gmail.com>
2019-05-15 15:14:22 +0900

    update

--- src/Components/Loader.js
+++ src/Components/Loader.js
@@ -1,22 +1,65 @@
 import React from "react";
-import styled from "styled-components";
+import styled, { keyframes } from "styled-components";
 
-const Container = styled.div`
-  height: 100vh;
-  width: 100vw;
+const Container = styled.div``;
+
+const Timer = styled.div`
   display: flex;
   justify-content: center;
-  font-size: 25px;
-  margin-top: 20px;
+  align-items: center;
+  width: 100vw;
+  margin-top: 30px;
 `;
 
-const Loader = () => (
+const boxBigger = keyframes`
+  0% {
+    transform: none;
+  }
+  20% {
+    transform: scale(1, 2);
+  }
+  50% {
+    transform: none;
+  }
+  100% {
+    transform: none;
+  }
+`;
+
+const MovingBox = styled.div`
+  width: 5px;
+  height: 30px;
+  background-color: rgba(255, 255, 255, 1);
+  margin: 3px;
+  animation: 1s ${boxBigger} infinite ease-in-out;
+
+  &:nth-child(1) {
+    animation-delay: 0.1s;
+  }
+  &:nth-child(2) {
+    animation-delay: 0.2s;
+  }
+  &:nth-child(3) {
+    animation-delay: 0.3s;
+  }
+  &:nth-child(4) {
+    animation-delay: 0.4s;
+  }
+  &:nth-child(5) {
+    animation-delay: 0.5s;
+  }
+`;
+
+const Loading = () => (
   <Container>
-    <span role="img" aria-label="Loading">
-      {/* 시각장애인을위해서 알려줌 */}
-      🕑
-    </span>
+    <Timer>
+      <MovingBox />
+      <MovingBox />
+      <MovingBox />
+      <MovingBox />
+      <MovingBox />
+    </Timer>
   </Container>
 );
 
-export default Loader;
+export default Loading;
